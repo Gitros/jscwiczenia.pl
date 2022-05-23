4a89bc3f254cd1c710f3c8d9ccc9ee8a4f3547c9
Gitros <31899790+Gitros@users.noreply.github.com>
2022-05-23 20:28:36 +0200

    zad13

---
+++ zad13/zad13.js
@@ -0,0 +1,17 @@
+const numberInput = document.getElementById('number-input');
+const displayBtn = document.getElementById('display-btn');
+const opis = document.getElementById('opis');
+
+const display = () => {
+    let p = document.createElement('p');
+    let liczba = parseInt(numberInput.value);
+    if (liczba>0){
+        p.textContent = liczba + " Liczba jest większa od 0";
+        opis.appendChild(p);
+    }
+    else{
+        p.textContent = liczba + "liczba jest ujemna lub 0"
+        opis.appendChild(p);
+    }
+}
+displayBtn.addEventListener('click', display)
